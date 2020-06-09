b798d315a9afd99a96b93e1d1507ae2bb2d79ad1
Liqin Zhang <graves_zhang@sjtu.edu.cn>
2020-06-09 22:01:36 +0800

    Add files via upload

---
+++ h3/cmake-build-debug/CMakeFiles/h3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h3.dir/aes.c.o"
+  "CMakeFiles/h3.dir/main.c.o"
+  "h3"
+  "h3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/h3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
